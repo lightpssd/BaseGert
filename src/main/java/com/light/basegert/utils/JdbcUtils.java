0fa60a24544d21deaa57c84dad0baf294a699e8c
Light <1175929958@qq.com>
2023-02-26 23:57:03 +0800

    as

--- src/main/java/com/light/basegert/utils/JdbcUtils.java
+++ src/main/java/com/light/basegert/utils/JdbcUtils.java
@@ -1,5 +1,6 @@
 package com.light.basegert.utils;
 
+import com.xiaoleilu.hutool.util.StrUtil;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.SQLSyntaxErrorException;
@@ -10,6 +11,7 @@ import java.util.function.Function;
 public class JdbcUtils {
 
     public static <T> T jdbcRunning(Function<JdbcTemplate, T> apply) {
+
         JdbcTemplate jdbcTemplate = SpringContextHolder.getBean(JdbcTemplate.class);
         return apply.apply(jdbcTemplate);
 
@@ -17,7 +19,9 @@ public class JdbcUtils {
 
 
     public static List<Map<String,Object>> jdbcQuerySql(String sql) throws SQLSyntaxErrorException {
-
+        if (StrUtil.isEmpty(sql)){
+            throw new SQLSyntaxErrorException("sql空语句");
+        }
         if (!sql.trim().startsWith("select")){
             throw new SQLSyntaxErrorException("非查询语句");
         }
